cc488c246fb672daff0c550d75cf05397f4cb42d
Vacuol <604008347@qq.com>
2018-12-18 17:50:27 +0800

    Revert "Revert "12.17""
    This reverts commit b06bc9c98826eee4b6542d0c05e2e09bd4bb2e7f.

---
+++ Control/Src/pid.c
@@ -0,0 +1,98 @@
+/**
+  *@file timer.c
+  *@date 2018-12-17
+  *@author Vacuo.W
+  *@brief 
+  */
+  
+#include "pid.h"
+#include "control.h"
+#include "can.h"
+#include "32mpu6050.h"
+
+/************CLOUD***************/
+PID_Regulator_t cloud_pitch_speed_pid;
+PID_Regulator_t cloud_pitch_position_pid;
+PID_Regulator_t cloud_yaw_speed_pid;
+PID_Regulator_t cloud_yaw_position_pid;
+
+void PID_Calc(PID_Regulator_t *pid)
+{
+	pid->err[0] = pid->err[1];
+	pid->err[1] = pid->ref - pid->fdb;
+	pid->inte += pid->err[1];
+		
+	
+	pid->componentKp  = pid->kp * pid->err[1];
+	pid->componentKi  = pid->ki * pid->inte;
+	pid->componentKd  = pid->kd * (pid->err[1] - pid->err[0]);
+	
+	if(pid->inte > 2000)//对积分项进行限制
+		pid->inte = 2000;
+	else if (pid->inte < -2000)
+		pid->inte = -2000;
+	
+//	if(pid->componentKp > pid->componentKpMax)
+//		pid->componentKp = pid->componentKpMax;
+//	else if (pid->componentKp < -pid->componentKpMax)
+//		pid->componentKp = -pid->componentKpMax;
+	
+	if(pid->componentKi > pid->componentKiMax)
+		pid->componentKi = pid->componentKiMax;
+	else if (pid->componentKi < -pid->componentKiMax)
+		pid->componentKi = -pid->componentKiMax;
+
+//	if(pid->componentKd > pid->componentKdMax)
+//		pid->componentKd = pid->componentKdMax;
+//	else if (pid->componentKd < -pid->componentKdMax)
+//		pid->componentKd = -pid->componentKdMax;
+	
+	
+	pid->output = pid->componentKp + pid->componentKi + pid->componentKd;
+	
+	if(pid->output > pid->outputMax)
+		pid->output = pid->outputMax;
+	else if (pid->output < -pid->outputMax)
+		pid->output = -pid->outputMax;	
+}
+
+void PID_Init(PID_Regulator_t *pid,float kp,float ki,float kd,float componentKiMax,float outputMax)
+{
+	pid->kp = kp;
+	pid->ki = ki;
+	pid->kd = kd;
+	pid->inte = 0;
+
+	pid->componentKiMax = componentKiMax;
+
+	pid->outputMax = outputMax;
+}
+
+void Cloud_Speed(void)
+{
+	//PITCH
+	cloud_pitch_speed_pid.fdb = sensor.gyro.radian.y;					//反馈参数：陀螺仪角速度
+	cloud_pitch_speed_pid.ref = cloud_pitch_position_pid.output;			//设定参数：位置环输出
+	PID_Calc(&cloud_pitch_speed_pid);
+	
+	//YAW
+	cloud_yaw_speed_pid.fdb = sensor.gyro.radian.x;					//反馈参数：陀螺仪角速度
+	cloud_yaw_speed_pid.ref = cloud_yaw_position_pid.output;			//设定参数：位置环输出
+	PID_Calc(&cloud_yaw_speed_pid);
+	
+}
+
+void Cloud_Position(void)
+{
+	//PITCH
+	cloud_pitch_position_pid.fdb = cloud_para[0].Bmechanical_angle;					//反馈参数:云台电机角度
+	cloud_pitch_position_pid.ref = pitch;										//设定参数
+	PID_Calc(&cloud_pitch_position_pid);
+	
+	//YAW
+	cloud_yaw_position_pid.fdb = cloud_para[1].Bmechanical_angle;					//反馈参数：云台电机角度
+	cloud_yaw_position_pid.ref = yaw;										//设定参数：位置环输出
+	PID_Calc(&cloud_yaw_position_pid);
+	
+	
+}
